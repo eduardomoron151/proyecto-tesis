2901b480d72632754bf21c45289cfe06c3922c31
Eduardo Moron <eduardomoron151@gmail.com>
2023-01-04 10:14:01 -0400

    actualizacion

--- bienvenida.php
+++ bienvenida.php
@@ -171,6 +171,27 @@
 
                     <?php endif; ?>
 
+                    <?php if($_SESSION['id_rol'] == 4): ?>
+
+                        <div class="row">
+
+                            <div class="col-lg-12">
+                                <div class="card shadow mb-4">
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">Enlaces de Interes</h6>
+                                    </div>
+                                    <div class="card-body">
+                                        The styling for this basic card example is created by using default Bootstrap
+                                        utility classes. By using utility classes, the style of the card component can be
+                                        easily modified with no need for any custom CSS!
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    <?php endif; ?>
+
                     
                 </div>
                 <!-- /.container-fluid -->
