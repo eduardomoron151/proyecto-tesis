fbd011275fdbed39fa8c70614e8c41e0cb79bd62
Eduardo Moron <eduardomoron151@gmail.com>
2023-01-04 10:18:27 -0400

    actualizacion

--- bienvenida.php
+++ bienvenida.php
@@ -181,9 +181,10 @@
                                         <h6 class="m-0 font-weight-bold text-primary">Enlaces de Interes</h6>
                                     </div>
                                     <div class="card-body">
-                                        The styling for this basic card example is created by using default Bootstrap
-                                        utility classes. By using utility classes, the style of the card component can be
-                                        easily modified with no need for any custom CSS!
+                                        <a href="https://www.bcv.org.ve/" target="_blank">Banco de Venezuela</a>
+                                        <a href="https://www.bcv.org.ve/" target="_blank">Banco de Venezuela</a>
+                                        <a href="https://www.bcv.org.ve/" target="_blank">Banco de Venezuela</a>
+
                                     </div>
                                 </div>
                             </div>
